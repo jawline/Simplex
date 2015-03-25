c0e85848a3276a70e5b7a9f094aa4adad777e1ce
Blake Loring <blake_l@parsed.uk>
2015-03-25 10:02:59 +0000

    Define grammar of parser in header

--- src/parser.h
+++ src/parser.h
@@ -1,4 +1,14 @@
 #ifndef _SIMPLEX_PARSER_DEF_H_
 #define _SIMPLEX_PARSER_DEF_H_
 
+/**
+ * ID = \[a-zA-Z]+[0-9]*\
+ * NUM = \[0-9]+(.[0-9]+)?\
+ * Grammar:
+ * MinMax: max Expression NEWLINE ['s.t.' Constraints]
+ * Constraints: Constraint [NEWLINE Constraints]
+ * Constraint: Expression '=' Expression
+ * Expression: (ID | NUM | NUM ID) [('+' | '-') Expression]
+ */
+
 #endif //_SIMPLEX_PARSER_DEF_H_
