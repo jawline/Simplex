c7567b9f92a0b0921be824c26885d37bfaedcf55
Blake Loring <blake_l@parsed.uk>
2015-03-25 09:44:22 +0000

    Steal skipWhitespace

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,12 @@
+#include "parser.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * Skip whitespace until the end of the input or the next character is found
+ */
+char const* nextToken(char const* input) {
+  for (; *input && isspace(*input); input++) {}
+  return input;
+}
