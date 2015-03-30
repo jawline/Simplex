22c8a8cf5cb2c19fa2d517c9220487dadcfa9353
Blake Loring <blake_l@parsed.uk>
2015-03-30 11:35:08 +0100

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -6,9 +6,33 @@
 /**
  * Skip whitespace until the end of the input or the next character is found
  */
-char const* nextToken(char const* input) {
+char const* skipWhite(char const* input) {
   for (; *input && isspace(*input); input++) {}
   return input;
+}
+
+/**
+ * Get the next token from the input string
+ */
+char const* nextToken(TOKEN* token, char const* input) {
+  input = skipWhite(input);
+  if (strncmp(input, "max", 3) == 0) {
+    *token = MAX;
+    return input + 3;
+  } else if (strncmp(input, "s.t.", 4) == 0) {
+    *token = ST;
+    return input + 4;
+  } else if (*input == '=') {
+    *token = EQ;
+    return input + 1;
+  } else if (*input == '+') {
+    *token = PLUS;
+    return input + 1;
+  } else if (*input == '-') {
+    *token = MINUS;
+    return input + 1;
+  }
+  return 0;
 }
 
 void parserInit() {}
