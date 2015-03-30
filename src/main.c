9c6ca388c77d3411363d12af226481f734af08ef
Blake Loring <blake_l@parsed.uk>
2015-03-31 00:04:18 +0100

    Takes max problem from cmdline

--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
 #include "parser.h"
 
 int main(int argc, char** argv) {
+
+	if (argc != 2) {
+		printf("Usage: %s max problem\n", argv[0]);
+		return -1;
+	}
+
 	parserInit();
-	parseString("max 5 s.t. 5x + 4x = 5");
+	parseString(argv[1]);
 	parserFree();
 }
