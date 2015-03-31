b4e963dc815ba13e411199ea8d8c61d2bb0a209b
Blake Loring <blake_l@parsed.uk>
2015-03-31 13:55:07 +0100

    Initial table implementation

--- src/tableau.h
+++ src/tableau.h
@@ -1,4 +1,14 @@
 #ifndef _SIMPLEX_TABLAEU_DEF_H_
 #define _SIMPLEX_TABLAEU_DEF_H_
+#include "column.h"
+#include "row.h"
+
+typedef struct {
+  column** columns;
+  unsigned int numColumns;
+  
+  row** rows;
+  unsigned int numRows;
+} table;
 
 #endif //_SIMPLEX_TABLAEU_DEF_H_
