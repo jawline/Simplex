1b320938c787ec42749893d1f1ce794c62548c51
Blake Loring <blake_l@parsed.uk>
2015-03-31 14:00:50 +0100

    Add some table helper definitions

--- src/tableau.h
+++ src/tableau.h
@@ -4,11 +4,18 @@
 #include "row.h"
 
 typedef struct {
-  column** columns;
+  column* columns;
   unsigned int numColumns;
   
   row** rows;
   unsigned int numRows;
 } table;
+
+void initialiseTable(table* instance);
+void freeTable(table* instance);
+
+int getTableColumnId(table* instance);
+column* getTableColumn(table* instance);
+int addTableColumn(char const* name, unsigned int nameLength);
 
 #endif //_SIMPLEX_TABLAEU_DEF_H_
