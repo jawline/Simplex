95a8570aa09ac6bba264670d39b10c519550d3bc
Blake Loring <blake_l@parsed.uk>
2015-04-01 10:49:54 +0100

    Docs on row helpers

--- src/row.h
+++ src/row.h
@@ -5,8 +5,19 @@ typedef struct {
   float* data;
 } row;
 
+/**
+ * Initialise a row
+ */
 void initialiseRow(row* instance, unsigned int numColumns);
+
+/**
+ * Expands the row size from numOldColumns to numNewColumns
+ */
 void expandRow(row* instance, unsigned int numOldColumns, unsigned int numNewColumns);
+
+/**
+ * Free any memory associated with a row
+ */
 void freeRow(row* instance);
 
 #endif //_ROW_DEF_H_
