26ecadc0ad98e2a65a00afb33295a073f0c39ee5
Blake Loring <blake_l@parsed.uk>
2015-04-02 10:56:13 +0100

    Docs on column

--- src/column.h
+++ src/column.h
@@ -8,7 +8,14 @@ typedef struct {
   char* name;
 } column;
 
+/**
+ * Initialise a column
+ */
 void initialiseColumn(column* column, char const* name, size_t nameLength);
+
+/**
+ * Free any memory associated with a column
+ */
 void freeColumn(column* column);
 
 #endif //_COLUMN_DEF_H_
