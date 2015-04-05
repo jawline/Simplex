ccf82f14bede9482f93bc34b0fe35dc0775e8ea8
Blake Loring <blake_l@parsed.uk>
2015-04-05 23:32:42 +0100

    Update solver.h

--- src/solver.h
+++ src/solver.h
@@ -3,12 +3,19 @@
 #include <stdbool.h>
 #include "tableau.h"
 
+/**
+ * Structure to hold results of simplex solver run
+ */
+typedef struct {
+  float value;
+} simplex_result;
+
 bool isBasic(table* instance, int col);
 
 /**
  * Solve the simplex tableau
  * NOTE: It is assumed that the last column in the table is the results column
  */
-void solveTable(table* instance);
+void solveTable(table* instance, simplex_result* results);
 
 #endif //_TABLEAU_SOLVER_DEF_H_
