d591086601aa3038c505873b669a560e897d4853
Blake Loring <blake_l@parsed.uk>
2015-04-05 12:39:58 +0100

    Add function to find the initial basic variables

--- src/solver.c
+++ src/solver.c
@@ -9,3 +9,24 @@ bool isBasic(table* instance, int col) {
 	}
 	return count > 0;
 }
+
+int findBasic(table* instance, int row) {
+	for (unsigned int i = 0; i < instance->numColumns; i++) {
+		if (isBasic(instance, i)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+void solveTable(table* instance) {
+	
+	//Find the initial basic variables (Only occur in one col)
+	int* rowBasicData = malloc(sizeof(int) * instance->numRows);
+	
+	for (unsigned int i = 0; i < instance->numRows; i++) {
+		rowBasicData[i] = findBasic(instance, i);
+	}
+
+	free(rowBasicData);
+}
